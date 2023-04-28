f31adef659fde508bfe4457fba7a23cd3c5829aa
Ben Berry-Allwood <b.berry-allwood@protonmail.com>
2023-04-28 14:19:09 +0100

    Set cmdheight to 0

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -6,7 +6,7 @@ vim.opt.tabstop = 2                             -- insert 2 spaces for a tab
 -- :help options
 vim.opt.backup = false                          -- creates a backup file
 vim.opt.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
-vim.opt.cmdheight = 2                           -- more space in the neovim command line for displaying messages
+vim.opt.cmdheight = 0                           -- more space in the neovim command line for displaying messages
 vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
 vim.opt.conceallevel = 0                        -- so that `` is visible in markdown files
 vim.opt.fileencoding = "utf-8"                  -- the encoding written to a file
